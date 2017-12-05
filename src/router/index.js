fd6c213fd2e09e0f3e8805353448147a21bea65c
Bartimeus <bartimeusurucskiy@gmail.com>
2017-12-05 12:35:53 +0200

    add filter by gender

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Basic from '@/components/Basic'
-import TablePager from '@/components/TablePager'
 
 Vue.use(Router)
 
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'Basic',
       component: Basic
-    },
-    {
-      path: '/search',
-      name: 'TablePager',
-      component: TablePager
     }
   ]
 })
